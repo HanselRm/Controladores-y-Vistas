72630be2ee42af674b4462e858e98ccf24f9b81f
HanselRm <20220897@itla.edu.do>
2024-01-22 23:30:44 -0400

    first

---
+++ Control&Vista/Controllers/BankController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Control_Vista.Controllers
+{
+    public class BankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();  
+        }
+        public IActionResult Depositar()
+        {
+            return View();
+        }
+
+        public IActionResult Dispensacion() 
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+    }
+}
